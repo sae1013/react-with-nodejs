c32525cc4e6593774635f911e1096582e22bbe85
sae1013 <sae1013@gmail.com>
2021-10-20 15:29:43 +0900

    Setting ProxyFile

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React,{useState,useEffect} from 'react';
 
 function App() {
+
+  const sendRequest = async() => {
+    const response = await axios.get('http://localhost:8080');
+    console.log(response);
+    console.log(response.data);
+  };
+
+  useEffect(()=>{
+    sendRequest();    
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
