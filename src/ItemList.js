e1a7ffc5cd831b813852bbf03d9c6202475d093c
sae1013 <sae1013@gmail.com>
2021-11-02 23:59:42 +0900

    Finish middle-component

--- src/ItemList.js
+++ src/ItemList.js
@@ -5,17 +5,24 @@ import classes from "./ItemList.module.css";
 
 function ItemList() {
   const [posts, setPosts] = React.useState([]);
-
+  const [userLikeList,setUserLikeList] = React.useState({});
   const sendRequest = async () => {
     const response = await axios.all([
       axios.get("https://jsonplaceholder.typicode.com/posts"),
-      axios.get("https://jsonplaceholder.typicode.com/posts"),
+      axios.get("http://localhost:8080/like/1/posts"),
     ]);
-    let [post,userLikeList] = [response[0].data,response[1].data]  // 좋아요리스트까지 불러와줘야함.
-    console.log(post,userLikeList)
-    // if (response.status) {
-    //   setPosts(response.data);
-    // }
+    let [posts,userLikeList] = [response[0].data,response[1].data]  // 좋아요리스트까지 불러와줘야함.
+    // 데이터 정제
+    
+    let formattedPost = posts.reduce((acc,post)=>{
+      const idx = userLikeList.post_list.post_list.findIndex((id)=> id ===`${post.id}` );
+      const formatPost =  (idx !== -1) ? {...post,isLike:true} : {...post,isLike:false}; 
+      acc.push(formatPost); 
+      return acc
+    },[]);
+    console.log(formattedPost)
+    setPosts(formattedPost);
+    setUserLikeList(userLikeList);
   };
 
   React.useEffect(() => {
